77fb8a4b843b5c8d96d4a1c0887b751df3077d0b
fredericlacroix <fredericlacroix@users.noreply.github.com>
2016-11-17 09:18:31 -0500

    script update

--- scripts/create_all_db.sh
+++ scripts/create_all_db.sh
@@ -2,6 +2,10 @@
 
 # This program creates the given databases using the create_db.sh script
 #
+# It takes two arguments:
+#   Name of the primary user
+#   Password of the primary user
+#
 # It prompts for the root password
 
-./create_db.sh country frederic bouchons
+./create_db.sh country $1 $2
